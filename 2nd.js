acc5642a74d432ede42b75f405c18db174079bf4
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-07-08 22:44:45 +0530

    Create 2nd.js

---
+++ 2nd.js
@@ -0,0 +1,8 @@
+console.log("Saksham")
+let a = 68
+console.log(a)
+a = "Saksham"
+console.log(a)
+
+// let g = 823sakshma error only used $, _, or letter as starting of variable
+// javascript is case sensitive Saksham and saksham are different variables
